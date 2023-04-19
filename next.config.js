4d26ac817aeba522f3ac8a02aa29ae79d0bae623
mrleidesen <mrleidesen@outlook.com>
2023-04-19 11:35:02 +0800

    chore: output config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   eslint: {
     dirs: ['pages', 'utils'],
   },
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
